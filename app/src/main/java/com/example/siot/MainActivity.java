bfad173b37bbb879677d0ed34da117641d31e8e2
Chirag chaudhari <chaudharichirag3006@gmail.com>
2023-09-21 17:36:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/siot/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.siot;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.siot.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.home_nav){
+                replaceFragment(new HomeFragment());
+            }else if (item.getItemId() == R.id.profile_nav){
+                replaceFragment(new ProfileFragment());
+            }else if (item.getItemId() == R.id.settings_nav){
+                replaceFragment(new SettingsFragment());
+            }
+
+            return true;
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.framelayout,fragment);
+        fragmentTransaction.commit();
+    }
+}
